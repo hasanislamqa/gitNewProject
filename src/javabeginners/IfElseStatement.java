9c528a7267001ab09a89a6d366faf45ed2501925
hasanurislam <hasanislamqa@gmail.com>
2019-05-17 21:47:43 -0400

    IfElseStatement created

---
+++ src/javabeginners/IfElseStatement.java
@@ -0,0 +1,21 @@
+package javabeginners;
+
+public class IfElseStatement {
+
+    static int Apple=60;
+    static int Orange=50;
+
+
+    public static void main(String[] args) {
+
+        if (Apple < 70){
+
+            System.out.println(Apple);
+        }else{
+
+            System.out.println(Orange);
+        }
+
+    }
+
+}
